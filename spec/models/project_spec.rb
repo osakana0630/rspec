7c4d7af761cfd149315f5180fb152aebac8c2568
osakana0630 <osakana0630@gmail.com>
2022-12-04 20:26:35 +0900

    project_spec追記

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,49 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
   it 'does not allow duplicate project names per user' do
     user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
+      first_name: 'taro',
+      last_name: 'tanaka',
+      email: 'test@test.com',
+      password: 'password'
     )
-
-    user.projects.create(
-      name: 'Test Project'
-    )
-
-    new_project = user.projects.build(
-      name: 'Test Project'
-    )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include('has already been taken')
+    user.projects.create(name: 'project name')
+    project = user.projects.build(name: 'project name')
+    project.valid?
+    expect(project).to_not be_valid
+    expect(project.errors[:name]).to include('has already been taken')
   end
 
+  # 2人のユーザーが同じプロジェクト名を使用することは許可すること
   it 'allows two users to share a project name' do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'joetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
+    user1 = User.create(
+      first_name: 'taro',
+      last_name: 'tanaka',
+      email: 'test1@test.com',
+      password: 'password'
     )
-
-    user.projects.create(
-      name: 'Test Project'
+    user2 = User.create(
+      first_name: 'jiro',
+      last_name: 'sato',
+      email: 'test2@test.com',
+      password: 'password'
     )
-
-    other_user = User.create(
-      first_name: 'Jane',
-      last_name: 'Tester',
-      email: 'janetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
-    )
-
-    other_project = other_user.projects.build(
-      name: 'Test Project'
-    )
-
-    expect(other_project).to be_valid
+    user1.projects.create(name: 'project name')
+    project = user2.projects.build(name: 'project name')
+    expect(project).to be_valid
   end
 end
