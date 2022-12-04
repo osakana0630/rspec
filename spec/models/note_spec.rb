e1fc2f57abce31c57102d3a593e0b4ad5c1fe1a6
osakana0630 <osakana0630@gmail.com>
2022-12-04 21:51:30 +0900

    note_spec リファクタリング

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,19 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  before do
+    @user = User.create!(
+      first_name: 'taro',
+      last_name: 'tanaka',
+      email: 'test@test.com',
+      password: 'password'
+    )
+    @project = @user.projects.create!(name: 'project name')
+  end
+
+  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること　
+  it 'is valid with a user, project and message' do
+    note = @project.notes.build(
+      message: 'test message',
+      user: @user
+    )
+    expect(note).to be_valid
+  end
+
+  # ユーザーがなければ無効な状態であること　
+  it 'is invalid without a user' do
+    note = @project.notes.build(
+      message: 'test message',
+      user: nil
+    )
+    expect(note).to_not be_valid
+  end
+
+  # プロジェクトがなければ無効な状態であること　
+  it 'is invalid without a project' do
+    note = Note.new(
+      message: 'test message',
+      user: @user,
+      project: nil
+    )
+    expect(note).to_not be_valid
+  end
+
+  # メッセージがなければ無効な状態であること　
+  it 'is invalid without a message' do
+    note = @project.notes.build(
+      message: nil,
+      user: @user
+    )
+    expect(note).to_not be_valid
+  end
+
   # searchスコープに関するテスト
   describe '.search' do
-    before(:all) do
-      user = User.create(
-        first_name: 'taro',
-        last_name: 'tanaka',
-        email: 'test@test.com',
-        password: 'password'
-      )
-      project = user.projects.create(name: 'project name')
-      @note1 = project.notes.create(message: 'This is the first note', user:)
-      @note2 = project.notes.create(message: 'This is the second note', user:)
-      @note3 = project.notes.create(message: 'First of all', user:)
+    before do
+      @note1 = @project.notes.create(message: 'This is the first note', user: @user)
+      @note2 = @project.notes.create(message: 'This is the second note', user: @user)
+      @note3 = @project.notes.create(message: 'First of all', user: @user)
     end
 
     # 一致するメモが見つかる場合
