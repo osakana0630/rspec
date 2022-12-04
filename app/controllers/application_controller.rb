739ddbb3a2146f17fff485fd69d1577133c5d4a6
osakana0630 <osakana0630@gmail.com>
2022-12-04 15:19:50 +0900

    rubocop適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
@@ -8,8 +10,8 @@ class ApplicationController < ActionController::Base
   end
 
   def project_owner?
-    unless @project.owner == current_user
-      redirect_to root_path, alert: "You don't have access to that project."
-    end
+    return if @project.owner == current_user
+
+    redirect_to root_path, alert: "You don't have access to that project."
   end
 end
