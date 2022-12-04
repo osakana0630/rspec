31299a7bc781ecbd8cd7ff78c219837f832036d2
osakana0630 <osakana0630@gmail.com>
2022-12-04 20:09:42 +0900

    user_spec追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,57 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name and email, and password' do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it 'is valid with a first_name, last_name, email, and password' do
     user = User.new(
-      first_name: 'Aaron',
-      last_name: 'Sumner',
-      email: 'tester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
+      first_name: 'taro',
+      last_name: 'tanaka',
+      email: 'test@test.com',
+      password: 'password'
     )
     expect(user).to be_valid
   end
 
-  it 'is invalid without a first name' do
-    user = User.new(first_name: nil)
+  # 名がないなら無効な状態であること
+  it 'is invalid without a first_name' do
+    user = User.new(
+      first_name: nil,
+      last_name: 'tanaka',
+      email: 'test@test.com',
+      password: 'password'
+    )
     user.valid?
+    expect(user).to_not be_valid
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
-  it 'is invalid without a last name' do
-    user = User.new(last_name: nil)
+  # 姓がないなら無効な状態であること
+  it 'is invalid without a last_name' do
+    user = User.new(
+      first_name: 'taro',
+      last_name: nil,
+      email: 'test@test.com',
+      password: 'password'
+    )
     user.valid?
+    expect(user).to_not be_valid
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-  it 'is invalid without an email address' do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it 'is invalid with a duplicate email address' do
+  # 重複したメールアドレスなら無効な状態であること
+  it 'is invalid with a duplicate email' do
     User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
+      first_name: 'taro',
+      last_name: 'tanaka',
+      email: 'test@test.com',
+      password: 'password'
     )
     user = User.new(
-      first_name: 'Jane',
-      last_name: 'Tester',
-      email: 'tester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze'
+      first_name: 'jiro',
+      last_name: 'hayashi',
+      email: 'test@test.com',
+      password: 'password'
     )
     user.valid?
+    expect(user).to_not be_valid
     expect(user.errors[:email]).to include('has already been taken')
-  end
-
-  it "returns a user's full name as a string" do
-    user = User.new(
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'johndoe@example.com'
-    )
-    expect(user.name).to eq 'John Doe'
   end
 end
