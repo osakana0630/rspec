890c1d87476415bd7463d0a49cb82f32dc768ef1
osakana0630 <osakana0630@gmail.com>
2022-12-04 21:07:31 +0900

    note_spec リファクタリング

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,34 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # 検索文字列に一致するメモを返すこと
-  it 'returns notes that match the search term' do
-    user = User.create(
-      first_name: 'taro',
-      last_name: 'tanaka',
-      email: 'test@test.com',
-      password: 'password'
-    )
-    project = user.projects.create(name: 'project name')
-    note1 = project.notes.create(message: 'This is the first note', user:)
-    note2 = project.notes.create(message: 'This is the second note', user:)
-    note3 = project.notes.create(message: 'First of all', user:)
-    expect(Note.search('first')).to include(note1, note3)
-    expect(Note.search('first')).to_not include(note2)
-  end
+  # searchスコープに関するテスト
+  describe '.search' do
+    before do
+      user = User.create(
+        first_name: 'taro',
+        last_name: 'tanaka',
+        email: 'test@test.com',
+        password: 'password'
+      )
+      project = user.projects.create(name: 'project name')
+      @note1 = project.notes.create(message: 'This is the first note', user:)
+      @note2 = project.notes.create(message: 'This is the second note', user:)
+      @note3 = project.notes.create(message: 'First of all', user:)
+    end
 
-  # 検索文字列に一致するメモが存在しないなら空の配列を返すこと
-  it 'returns empty collection when no results are found' do
-    user = User.create(
-      first_name: 'taro',
-      last_name: 'tanaka',
-      email: 'test@test.com',
-      password: 'password'
-    )
-    project = user.projects.create(name: 'project name')
-    project.notes.create(message: 'This is the first note', user:)
-    project.notes.create(message: 'This is the second note', user:)
-    project.notes.create(message: 'First of all', user:)
-    expect(Note.search('third')).to be_empty
+    # 一致するメモが見つかる場合
+    context 'when a match is found' do
+      # 検索文字列に一致するメモを返すこと
+      it 'returns notes that match the search term' do
+        expect(Note.search('first')).to include(@note1, @note3)
+        expect(Note.search('first')).to_not include(@note2)
+      end
+    end
+
+    # 一致するメモが見つからない場合
+    context 'when no match is found' do
+      # 検索文字列に一致するメモが存在しないなら空の配列を返すこと
+      it 'returns empty collection when no results are found' do
+        expect(Note.search('third')).to be_empty
+      end
+    end
   end
 end
